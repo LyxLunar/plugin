c757bef030ff7ab3ce99b127a4ffdf294416959a
Atlas <rusuluca552@gmail.com>
2026-08-16 13:26:57 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 ({
     onLoad() {
-        console.log("CloudCord Cosmetics loaded");
+        console.log("CloudCord Cosmetics settings test loaded");
     },
 
     onUnload() {
-        console.log("CloudCord Cosmetics unloaded");
+        console.log("CloudCord Cosmetics settings test unloaded");
     },
 
-    settings: null
+    settings: function Settings() {
+        const React = vendetta.common.React;
+        const RN = vendetta.common.ReactNative;
+
+        return React.createElement(
+            RN.View,
+            { style: { flex: 1, padding: 20 } },
+            React.createElement(
+                RN.Text,
+                { style: { fontSize: 24, fontWeight: "800" } },
+                "CloudCord Cosmetics"
+            ),
+            React.createElement(
+                RN.Text,
+                { style: { marginTop: 12, fontSize: 16 } },
+                "Settings are working!"
+            )
+        );
+    }
 })
