7b7b1bc5343e13522d1b7dbf10dd2c86499f7b0b
Atlas <rusuluca552@gmail.com>
2026-08-16 13:35:45 +0300

    Refactor logging and remove settings function

--- index.js
+++ index.js
@@ -1,46 +1,11 @@
 vendetta => ({
     onLoad() {
-        vendetta.logger.info("CloudCord Cosmetics settings test loaded");
+        console.log("CloudCord Cosmetics loaded");
     },
 
     onUnload() {
-        vendetta.logger.info("CloudCord Cosmetics settings test unloaded");
+        console.log("CloudCord Cosmetics unloaded");
     },
 
-    settings: function Settings() {
-        const React = vendetta.common.React;
-        const ReactNative = vendetta.common.ReactNative;
-
-        return React.createElement(
-            ReactNative.View,
-            {
-                style: {
-                    flex: 1,
-                    padding: 20
-                }
-            },
-
-            React.createElement(
-                ReactNative.Text,
-                {
-                    style: {
-                        fontSize: 24,
-                        fontWeight: "800"
-                    }
-                },
-                "CloudCord Cosmetics"
-            ),
-
-            React.createElement(
-                ReactNative.Text,
-                {
-                    style: {
-                        marginTop: 12,
-                        fontSize: 16
-                    }
-                },
-                "Settings are working!"
-            )
-        );
-    }
+    settings: null
 })
