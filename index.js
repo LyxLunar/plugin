18bc2649ba63f7aba47fb3bdb61d3e7757763cbd
Atlas <rusuluca552@gmail.com>
2026-08-16 13:30:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
-({
-    onLoad() {
-        console.log("CloudCord Cosmetics settings test loaded");
+const plugin = {
+    SettingsComponent() {
+        return null;
     },
 
-    onUnload() {
-        console.log("CloudCord Cosmetics settings test unloaded");
+    start() {
+        bunny.plugin.logger.info("CloudCord Cosmetics settings test loaded");
     },
 
-    settings: function Settings() {
-        const React = vendetta.common.React;
-        const RN = vendetta.common.ReactNative;
-
-        return React.createElement(
-            RN.View,
-            { style: { flex: 1, padding: 20 } },
-            React.createElement(
-                RN.Text,
-                { style: { fontSize: 24, fontWeight: "800" } },
-                "CloudCord Cosmetics"
-            ),
-            React.createElement(
-                RN.Text,
-                { style: { marginTop: 12, fontSize: 16 } },
-                "Settings are working!"
-            )
-        );
+    stop() {
+        bunny.plugin.logger.info("CloudCord Cosmetics settings test unloaded");
     }
-})
+};
