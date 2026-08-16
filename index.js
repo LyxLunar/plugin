0c90b07577c432f7f6e27b9103defd305b3c50db
Atlas <rusuluca552@gmail.com>
2026-08-16 13:44:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
 vendetta => ({
     onLoad() {
-        try {
-            const Avatar = vendetta.metro.common.Avatar;
-
-            vendetta.logger.info(
-                "[Cosmetics] Avatar module found: " +
-                Object.keys(Avatar || {}).join(", ")
-            );
-
-            if (Avatar && Avatar.default) {
-                vendetta.logger.info(
-                    "[Cosmetics] Avatar.default found: " +
-                    (Avatar.default.name || "anonymous")
-                );
-            }
-        } catch (e) {
-            vendetta.logger.error(
-                "[Cosmetics] Avatar probe failed: " +
-                String(e)
-            );
-        }
+        console.log("CloudCord Cosmetics loaded");
     },
 
     onUnload() {
-        vendetta.logger.info("[Cosmetics] Avatar probe unloaded");
+        console.log("CloudCord Cosmetics unloaded");
     },
 
     settings: () => null
