0bc91a86a47837c7a1f966e8b03632bc1035e996
Atlas <rusuluca552@gmail.com>
2026-08-16 13:32:29 +0300

    Implement SettingsComponent for CloudCord Cosmetics

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 const plugin = {
     SettingsComponent() {
-        return null;
+        const React = bunny.api.react.React;
+        const ReactNative = bunny.api.react.ReactNative;
+
+        return React.createElement(
+            ReactNative.View,
+            {
+                style: {
+                    flex: 1,
+                    padding: 20
+                }
+            },
+            React.createElement(
+                ReactNative.Text,
+                {
+                    style: {
+                        fontSize: 24,
+                        fontWeight: "800"
+                    }
+                },
+                "CloudCord Cosmetics"
+            ),
+            React.createElement(
+                ReactNative.Text,
+                {
+                    style: {
+                        marginTop: 12,
+                        fontSize: 16
+                    }
+                },
+                "Settings are working!"
+            )
+        );
     },
 
     start() {
-        bunny.plugin.logger.info("CloudCord Cosmetics settings test loaded");
+        bunny.plugin.logger.info("CloudCord Cosmetics loaded");
     },
 
     stop() {
-        bunny.plugin.logger.info("CloudCord Cosmetics settings test unloaded");
+        bunny.plugin.logger.info("CloudCord Cosmetics unloaded");
     }
 };
